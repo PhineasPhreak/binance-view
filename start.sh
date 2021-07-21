4ed23452105a99fc11dca612a9b9bce660ddfda3
phineasphreak <riftpp@gmail.com>
2021-07-21 20:54:53 +0200

    typo command

--- start.sh
+++ start.sh
@@ -3,16 +3,16 @@
 # Github : https://github.com/PhineasPhreak/binance-view
 #
 # Create virtual environment with python3 and pip
-python3 -m pip venv .env || \
-echo "Error during the creation of the virtual environment"
+python3 -m venv .env || \
+echo "Error during the creation of the virtual environment" ; exit 1
 
 # Make '.env' my environment
 source .env/bin/activate || \
-echo "Error with the 'source' command"
+echo "Error with the 'source' command" ; exit 1
 
 # Install Python packages with pip and requirements.txt
 pip install -r requirements.txt || \
-echo "Error during package installation with pip and 'requirements.txt' file"
+echo "Error during package installation with pip and 'requirements.txt' file" ; exit 1
 
 # Create config.py file for configuration for API KEY
 # See README.md file for more information
