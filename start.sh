5ff4417241bc8a5866086f65a4a33b7047b830b6
phineasphreak <riftpp@gmail.com>
2021-07-23 10:51:29 +0200

    show virtual env and indent

--- start.sh
+++ start.sh
@@ -32,20 +32,21 @@ else
     # Create virtual environment with python3 and pip
     echo "[+] Create virtual environment..."
     python3 -m venv $env || \
-    echo "Error during the creation of the virtual environment" || exit 1
+        echo "Error during the creation of the virtual environment" || exit 1
 fi
 sleep 1
 
 # Make '.env' my environment
 echo "[+] Choose '$env' like environment"
 source .env/bin/activate || \
-echo "Error with the 'source' command" || exit 1
+    echo "Error with the 'source' command" || exit 1
+echo Env: $VIRTUAL_ENV
 sleep 1
 
 # Install Python packages with pip and requirements.txt
 echo "[+] Install packages with pip3"
 pip3 install -r $requirements || \
-echo "Error during package installation with pip and 'requirements.txt' file" || exit 1
+    echo "Error during package installation with pip and 'requirements.txt' file" || exit 1
 sleep 1
 
 # Create config.py file for configuration for API KEY
